f51f7648ec95c4cdbe82123b2e577b866892c9ec
monilpat <monilpat@gmail.com>
2018-11-08 21:37:20 -0800

    Spacing fixes

--- src/js/components/CreateTripPage.js
+++ src/js/components/CreateTripPage.js
@@ -5,38 +5,35 @@ import { colors } from '../styles/index'
 const Wrapper = styled.div`
   text-align: center;
 `
-
 const TripNameInput = styled.form`
   text-align: left;
   display: flex;
   flex-direction: column;
   text-align: center;
 `
-const SubmitButton = styled.input` 
+const SubmitButton = styled.input`
   color: ${colors.black};
   background-color: ${colors.green};
-
 `
 const FormInput = styled.input`
   border-style: solid;
   margin-bottom: 75px;
-
 `
-
 const PageTitle = styled.div``
 const CreateTripPage = () => {
   return (
     <Wrapper>
-      Create Trip 
+      Create Trip
       <TripNameInput>
         Title: <FormInput type="text" name="title" />
         Place: <FormInput type="text" name="place" />
         Start Date: <FormInput type="date" name="startDate" />
         End Date: <FormInput type="date" name="endDate" />
         Upload Media: <FormInput type="file" name="media" />
-        <SubmitButton type="submit" value="Submit" multiple/>
+        <SubmitButton type="submit" value="Submit" multiple />
       </TripNameInput>
     </Wrapper>
   )
 }
+
 export default CreateTripPage
