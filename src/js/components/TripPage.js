c803bcfd4a2a05e637334e7ac3ecf1c01ffa1d6c
Simon Zhou <simon.zhou@ucla.edu>
2018-11-09 02:18:23 -0800

    Added documentation page

--- src/js/components/TripPage.js
+++ src/js/components/TripPage.js
@@ -2,15 +2,34 @@ import React from 'react'
 import styled from 'styled-components'
 import SearchBar from './SearchBar'
 import MapOverview from './MapOverview'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 
 import mock from '../mocks/mock'
+import { colors } from '../styles'
 
 const TripPageContainer = styled.div`
-  display: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100wh;
+  height: 100vh;
 
   > * {
     margin-bottom: 20px;
   }
+`
+
+const TripPageMapWrapper = styled.div`
+  position: relative;
+  height: 40%;
+  width: 100%;
+`
+
+const TripPageAddMemory = styled.div`
+  position: fixed;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
 `
 
 const TripPage = props => {
@@ -19,8 +38,13 @@ const TripPage = props => {
 
   return (
     <TripPageContainer>
-      <SearchBar placeholder={'Untitled Trip'} />
-      <MapOverview />
+      <SearchBar placeholder={'A Splendid Summer on Saturn'} />
+      <TripPageMapWrapper>
+        <MapOverview />
+      </TripPageMapWrapper>
+      <TripPageAddMemory>
+        <FontAwesomeIcon icon={faPlusCircle} size="5x" color={colors.green} />
+      </TripPageAddMemory>
     </TripPageContainer>
   )
 }
