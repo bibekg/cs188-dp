9806765c2d050851aa2caf5593202716d8c7ebff
Simon Zhou <simon.zhou@ucla.edu>
2018-11-27 19:46:54 -0800

    Added firebase

---
+++ src/js/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+const config = {
+  apiKey: 'AIzaSyAPigSypdfSb6r4AX9SkNoEXjDZEz7-zV8',
+  authDomain: 'memoricle-1541754630896.firebaseapp.com',
+  databaseURL: 'https://memoricle-1541754630896.firebaseio.com',
+  projectId: 'memoricle-1541754630896',
+  storageBucket: 'memoricle-1541754630896.appspot.com',
+  messagingSenderId: '123773528711'
+}
+firebase.initializeApp(config)
+export default firebase
