472393dfc771852233f9d43b64bfa95d81d165fc
Bibek <ghim.bibek@gmail.com>
2018-12-02 22:25:54 -0800

    Fix date-less exif parsing bug

--- src/js/util/datetime.ts
+++ src/js/util/datetime.ts
@@ -1,7 +1,11 @@
 export const parseExifDateTime = (exifDateTime: string): Date | null => {
+  if (exifDateTime == null) return null
   const [datePart, timePart] = exifDateTime.split(' ')
+  if (!Array.isArray(datePart) || !Array.isArray(timePart)) return null
   const [year, month, date] = datePart.split(':').map(x => +x)
+  if (year == null || month == null || date == null) return null
   const [hour, minute, second] = timePart.split(':').map(x => +x)
+  if (hour == null || month == null || second == null) return null
 
   const d = new Date()
   d.setFullYear(year, month - 1, date)
