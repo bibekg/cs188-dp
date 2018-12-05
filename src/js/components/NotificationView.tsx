d679e85d79372041962f8f8e0a65684f2605ca99
Bibek <ghim.bibek@gmail.com>
2018-12-04 20:03:32 -0800

    NotificationView improvements - Make notification pop up after 3 seconds - Make logo just an M - Change text to be more of a call-to-action

--- src/js/components/NotificationView.tsx
+++ src/js/components/NotificationView.tsx
@@ -6,7 +6,6 @@ import { Link } from 'react-router-dom'
 import { colors } from '../styles/index'
 
 import background from '../../assets/notification-background.jpeg'
-import titleIcon from '../../assets/sun.png'
 import { rgba } from 'polished'
 
 const ImageWrapper = styled.div`
@@ -17,60 +16,104 @@ const ImageWrapper = styled.div`
   justify-content: center;
   align-items: center;
 `
+
 const Image = styled.img`
   height: 100%;
   width: 100%;
   object-fit: cover;
 `
+
+const Logo = styled.div`
+  height: 20px;
+  width: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${colors.white};
+  background-color: ${colors.green};
+  border-radius: 5px;
+`
+
 const NotificationContainer = styled.div`
   box-shadow: 0 1px 6px 0 ${rgba(colors.mostlyBlack, 0.28)};
   width: calc(100% - 10px);
   position: fixed;
-  top: 5px;
+  top: ${props => (props.showing ? 5 : -200)}px;
+  transition: 0.5s ease top;
   left: 5px;
   padding: 15px;
   border-radius: 5px;
   background: ${colors.offWhite};
-  img {
-    width: 30px;
-    height: 30px;
-  }
 `
 const Header = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-bottom: 10px;
 `
 
 const HeaderLeft = styled.div`
   display: flex;
   align-items: center;
+  & > :first-child {
+    margin-right: 5px;
+  }
 `
 
-const NotificationView = props => {
-  return (
-    <div>
-      <Link
-        style={{ textDecoration: 'none' }}
-        to="/trip/cb171710-f471-11e8-b405-895a5285bb23/edit"
-      >
-        <NotificationContainer>
-          <Header>
-            <HeaderLeft>
-              <img src={titleIcon} /> {'   '}
-              <Text> MEMORICLE</Text>
-            </HeaderLeft>
-            <Text>now</Text>
-          </Header>
-          <Text bold>Add a photo to your trip!</Text>
-          <Text>Would you link to add to your trip "A Week in Utah"?</Text>
-        </NotificationContainer>
-      </Link>
-      <ImageWrapper>
-        <Image src={background} alt="background" />
-      </ImageWrapper>
-    </div>
-  )
+interface PropsType {}
+
+interface StateType {
+  showing: boolean
+}
+
+class NotificationView extends React.Component<PropsType, StateType> {
+  state = {
+    showing: false
+  }
+
+  constructor(props: PropsType) {
+    super(props)
+    this.state = {
+      showing: false
+    }
+  }
+
+  componentDidMount() {
+    window.setTimeout(() => {
+      this.setState({
+        showing: true
+      })
+    }, 3000)
+  }
+
+  render() {
+    return (
+      <div>
+        <Link
+          style={{ textDecoration: 'none' }}
+          to="/trip/cb171710-f471-11e8-b405-895a5285bb23/edit"
+        >
+          <NotificationContainer showing={this.state.showing}>
+            <Header>
+              <HeaderLeft>
+                <Logo>M</Logo>
+                <Text> MEMORICLE</Text>
+              </HeaderLeft>
+              <Text>now</Text>
+            </Header>
+            <Text bold>Add a photo to your trip!</Text>
+            <Text>
+              You haven't added too many photos to your "A Week in Utah" trip
+              yet. Add one now to get the ball rolling.
+            </Text>
+          </NotificationContainer>
+        </Link>
+        <ImageWrapper>
+          <Image src={background} alt="background" />
+        </ImageWrapper>
+      </div>
+    )
+  }
 }
 
 export default NotificationView
